7066aee9c2784aa9f4dc64e30d9a0fccd765e513
leonardkc <ndulueleonard@gmail.com>
2022-11-12 17:59:52 +0100

    Update django task

--- practice/task/views.py
+++ practice/task/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+task = ["foo", "bar", "baz"]
+
 # Create your views here.
+def index(request):
+    return render(request, "task/index.html", {
+        "task": task
+    })
+
+def add(request):
+    return render(request, "task/add.html")
