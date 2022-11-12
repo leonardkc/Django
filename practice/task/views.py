61e680721da7b81ba8fd4aaeaa13384a5d25456c
leonardkc <ndulueleonard@gmail.com>
2022-11-12 19:44:30 +0100

    Update django task

--- practice/task/views.py
+++ practice/task/views.py
@@ -1,6 +1,11 @@
+from django import forms
 from django.shortcuts import render
 
 task = ["foo", "bar", "baz"]
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 # Create your views here.
 def index(request):
@@ -9,4 +14,15 @@ def index(request):
     })
 
 def add(request):
-    return render(request, "task/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            task.append(task)
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "task/add.html", {
+        "form":NewTaskForm()
+    })
